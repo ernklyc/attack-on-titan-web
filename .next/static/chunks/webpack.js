06d890ea2c91f80e181ef62218c5411452b9f27d
ernklyc <ernklyc@gmail.com>
2025-05-03 18:55:48 +0300

    Add hot update files for layout and webpack with eval-source-map configuration
    - Created hot-update JSON for layout and webpack.
    - Added hot-update JavaScript files for app layout and webpack, including source maps for development.
    - Included globals.css in the layout hot update.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("008231a53a42cf69")
+/******/ 		__webpack_require__.h = () => ("001bdb444abf5e18")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
